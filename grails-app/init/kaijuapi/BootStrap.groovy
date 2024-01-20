0b30b72601fa30b49577d386bf58b28028bf4537
mrp4sten <mauricio.pasten.martinez@gmail.com>
2024-01-20 15:57:45 -0600

    Add new Kaiju, Movie, and Battle objects to BootStrap.groovy

--- grails-app/init/kaijuapi/BootStrap.groovy
+++ grails-app/init/kaijuapi/BootStrap.groovy
@@ -3,7 +3,20 @@ package kaijuapi
 class BootStrap {
 
     def init = { servletContext ->
-        new Kaiju(name: "Godzilla", img: "https://upload.wikimedia.org/wikipedia/en/2/2a/Godzilla_%282014%29_poster.jpg", ability: "Atomic Breath", firstAppearance: 1954).save()
+        Kaiju godzilla = new Kaiju(name: "Godzilla", img: "https://wikizilla.org/w/images/3/30/Godzilla_Minus_One_version.png", ability: "Atomic Breath", firstAppearance: 1954).save()
+        Kaiju kong =new Kaiju(name: "Kong", img: "https://static.wikia.nocookie.net/legendary-monsterverse/images/9/9a/GvK_Kong.png", ability: "Battle Axe", firstAppearance: 1933).save()
+
+        List<Kaiju> kaijus = [godzilla, kong]
+
+        Movie movie = new Movie(title: "Godzilla Minus One", img: "https://upload.wikimedia.org/wikipedia/en/5/5a/Godzilla_%282014%29_poster.jpg", releaseYear: 2023, director: "Takashi Yamazaki")
+        movie.addToKaijus(godzilla)
+        movie.save()
+        
+        Battle battle = new Battle(location: "Tokyo", winner: godzilla, loser: kong)
+        battle.addToKaijus(godzilla)
+        battle.addToKaijus(kong)
+        battle.save()
+
     }
     def destroy = {
     }
