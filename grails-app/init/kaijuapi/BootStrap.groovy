2f33669bef6b10bd026c19cadf422d20b51978ce
mrp4sten <mauricio.pasten.martinez@gmail.com>
2024-01-20 00:14:06 -0600

    Add Godzilla to BootStrap.groovy

--- grails-app/init/kaijuapi/BootStrap.groovy
+++ grails-app/init/kaijuapi/BootStrap.groovy
@@ -3,6 +3,7 @@ package kaijuapi
 class BootStrap {
 
     def init = { servletContext ->
+        new Kaiju(name: "Godzilla", img: "https://upload.wikimedia.org/wikipedia/en/2/2a/Godzilla_%282014%29_poster.jpg", ability: "Atomic Breath", firstAppearance: 1954).save()
     }
     def destroy = {
     }
